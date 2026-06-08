2eb13f85587f701a64be94ef0bde6c9d7ee051ce
Seu Nome <gustavo.toigo@gmail.com>
2026-06-08 02:04:59 -0300

    x

---
+++ js/constants.js
@@ -0,0 +1,19 @@
+        const SITE_VERSION = "v2.2.0"; 
+        const USER_NICK    = "Nahar.5349";       
+        const USER_EMAIL   = "gustavo.toigo@gmail.com";
+
+        const ACHIEVEMENTS = {
+            SAVANT: 4001,
+            PRODIGY: 4015,
+            CHAMPION: 3990,
+            GOD: 4018
+        };
+
+        const WALLET_IDS = {
+            RELICS: 7,
+            PRISTINE: 24
+        };
+
+        const MATERIAL_IDS = {
+            MATRIX: 79230
+        };
